ff6e8185dd41827270466ce8c019ba99c3c1a851
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-11 20:49:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const list = document.getElementById("tasks");
+  const input = document.getElementById("new-task-description");
+
+  document.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    let listItem = document.createElement("li");
+    let deleteBtn = document.createElement("Button");
+
+    deleteBtn.innerHTML = "X";
+    listItem.innerHTML = input.value;
+    listItem.id = `${index}`
+
+    list.appendChild(listItem);
+    listItem.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", ()=>{
+      deleteBtn.parentElement.remove();
+    })
+
+    input.value = ""
+
+  });
+
 });
